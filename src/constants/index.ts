b57a8248fc5383fa5e32f25a467829eee5bb7ba1
Felipe Alves <felipe.contato.a@gmail.com>
2024-06-03 06:23:31 -0300

    add(constants): change INITIAL_MK_CONTENT

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,77 +1,83 @@
 export const AUTO_SAVE_KEY = "mk-auto-save";
 export const MK_LIST_KEY = "mk-documents";
 
-export const INITIAL_MK_CONTENT = `# Markdown syntax guide
+export const INITIAL_MK_CONTENT = `# Hello World
 
-## Headers
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla accumsan, metus ultrices eleifend gravida, nulla nunc varius lectus, nec rutrum justo nibh eu lectus. Ut vulputate semper dui. Fusce erat odio, sollicitudin vel erat vel, interdum mattis neque. Subscript works as well!
 
-# This is a Heading h1
-## This is a Heading h2
-### This is a Heading h3
-#### This is a Heading h4
-##### This is a Heading h5
-###### This is a Heading h6
+## Second level
 
-## Emphasis
+Curabitur accumsan turpis pharetra **augue tincidunt** blandit. Quisque condimentum maximus mi, sit amet commodo arcu rutrum id. Proin pretium urna vel cursus venenatis. Suspendisse potenti. Etiam mattis sem rhoncus lacus dapibus facilisis. Donec at dignissim dui. Ut et neque nisl.
 
-*This text will be italic*  
-_This will also be italic_
+- In fermentum leo eu lectus mollis, quis dictum mi aliquet.
+- Morbi eu nulla lobortis, lobortis est in, fringilla felis.
+- Aliquam nec felis in sapien venenatis viverra fermentum nec lectus.
+- Ut non enim metus.
 
-**This text will be bold**  
-__This will also be bold__
+### Third level
 
-_You **can** combine them_
+Quisque ante lacus, malesuada ac auctor vitae, congue non ante. Phasellus lacus ex, semper ac tortor nec, fringilla condimentum orci. Fusce eu rutrum tellus.
 
-## Lists
+1. Donec blandit a lorem id convallis.
+2. Cras gravida arcu at diam gravida gravida.
+3. Integer in volutpat libero.
+4. Donec a diam tellus.
+5. Aenean nec tortor orci.
+6. Quisque aliquam cursus urna, non bibendum massa viverra eget.
+7. Vivamus maximus ultricies pulvinar.
 
-### Unordered
+> Ut venenatis, nisl scelerisque sollicitudin fermentum, quam libero hendrerit ipsum, ut blandit est tellus sit amet turpis.
 
-* Item 1
-* Item 2
-* Item 2a
-* Item 2b
+Quisque at semper enim, eu hendrerit odio. Etiam auctor nisl et *justo sodales* elementum. Maecenas ultrices lacus quis neque consectetur, et lobortis nisi molestie.
 
-### Ordered
+Sed sagittis enim ac tortor maximus rutrum. Nulla facilisi. Donec mattis vulputate risus in luctus. Maecenas vestibulum interdum commodo.
 
-1. Item 1
-2. Item 2
-3. Item 3
-    1. Item 3a
-    2. Item 3b
+[Web](https://google.com/)
+: The part of the Internet that contains websites and web pages
 
-## Images
+[HTML](https://developer.mozilla.org/en-US/docs/Web/HTML)
+: A markup language for creating web pages
 
-![This is an alt text.](https://markdownlivepreview.com/image/sample.webp "This is a sample image.")
+[CSS](https://developer.mozilla.org/en-US/docs/Web/CSS)
+: A technology to make HTML look better
 
-## Links
+Suspendisse egestas sapien non felis placerat elementum. Morbi tortor nisl, suscipit sed mi sit amet, mollis malesuada nulla. Nulla facilisi. Nullam ac erat ante.
 
-You may be using [Markdown Live Preview](https://markdownlivepreview.com/).
+#### Fourth level
 
-## Blockquotes
-
-> Markdown is a lightweight markup language with plain-text-formatting syntax, created in 2004 by John Gruber with Aaron Swartz.
->
->> Markdown is often used to format readme files, for writing messages in online discussion forums, and to create rich text using a plain text editor.
-
-## Tables
-
-| Left columns  | Right columns |
-| ------------- |:-------------:|
-| left foo      | right foo     |
-| left bar      | right bar     |
-| left baz      | right baz     |
-
-## Blocks of code
+Nulla efficitur eleifend nisi, sit amet bibendum sapien fringilla ac. Mauris euismod metus a tellus laoreet, at elementum ex efficitur.
 
 \`\`\`
-let message = 'Hello world';
-alert(message);
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Hello World</title>
+    </head>
+    <body>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec viverra nec nulla vitae mollis.</p>
+    </body>
+</html>
 \`\`\`
 
-## Inline code
+Maecenas eleifend sollicitudin dui, faucibus sollicitudin augue cursus non. Ut finibus eleifend arcu ut vehicula. Mauris eu est maximus est porta condimentum in eu justo. Nulla id iaculis sapien.
 
-This web site is using \`markedjs/marked\`.
+| One    | Two    |
+| ------ | ------ |
+| Three  | Four   |
+| Five   | Six    |
+| Seven  | Eight  |
+| Nine   | Ten    |
+| Eleven | Twelve |
 
+Phasellus porttitor enim id metus volutpat ultricies. Ut nisi nunc, blandit sed dapibus at, vestibulum in felis. Etiam iaculis lorem ac nibh bibendum rhoncus. Nam interdum efficitur ligula sit amet ullamcorper. Etiam tristique, leo vitae porta faucibus, mi lacus laoreet metus, at cursus leo est vel tellus. Sed ac posuere est. Nunc ultricies nunc neque, vitae ultricies ex sodales quis. Aliquam eu nibh in libero accumsan pulvinar. Nullam nec nisl placerat, pretium metus vel, euismod ipsum. Proin tempor cursus nisl vel condimentum. Nam pharetra varius metus non pellentesque.
+
+##### Fifth level
+
+Aliquam sagittis rhoncus vulputate. Cras non luctus sem, sed tincidunt ligula. Vestibulum at nunc elit. Praesent aliquet ligula mi, in luctus elit volutpat porta. Phasellus molestie diam vel nisi sodales, a eleifend augue laoreet. Sed nec eleifend justo. Nam et sollicitudin odio.
+
+###### Sixth level
+
+Cras in nibh lacinia, venenatis nisi et, auctor urna. Donec pulvinar lacus sed diam dignissim, ut eleifend eros accumsan. Phasellus non tortor eros. Ut sed rutrum lacus. Etiam purus nunc, scelerisque quis enim vitae, malesuada ultrices turpis. Nunc vitae maximus purus, nec consectetur dui. Suspendisse euismod, elit vel rutrum commodo, ipsum tortor maximus dui, sed varius sapien odio vitae est. Etiam at cursus metus.
 `;
 
 export const THEME_KEY = "mk-theme";
